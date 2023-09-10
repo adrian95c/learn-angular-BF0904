46c4dba1adb1b20445acbab870062932cd3b66ab
Adrian <adrian_chin95@outlook.com>
2023-09-10 13:26:06 +0800

    Understand component & AppModule.
    Create new component, and component declaration.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //It tells Angular to create and insert an instance of this component wherever it finds the corresponding tag in template HTML. For example, if an application's HTML contains <app-root></app-root> , then Angular inserts an instance of the AppComponent view between those tags.
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Angular will not scan through all component files in your app folder, unless you have register/ import here
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule
