262bef4b44488b0470162c7fb3b383bbfe7aa249
Adrian <adrian_chin95@outlook.com>
2023-09-10 14:11:27 +0800

    Use custom component.
    Create component with command, and nesting components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 // Angular will not scan through all component files in your app folder, unless you have register/ import here
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core"; // It allows to access core functions of Angular
+
+@Component({
+    selector:'app-server',
+    templateUrl: './server.component.html'
+})
+export class ServerComponent {
+
+}
