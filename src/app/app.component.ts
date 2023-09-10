d8a54079c48c54c9ba03c586f438e8b2f12eadb0
Adrian <adrian_chin95@outlook.com>
2023-09-10 15:02:26 +0800

    Work on component template, and style.
    Use different formatting to use template, and style.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root', //It tells Angular to create and insert an instance of this component wherever it finds the corresponding tag in template HTML. For example, if an application's HTML contains <app-root></app-root> , then Angular inserts an instance of the AppComponent view between those tags.
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html', // either 'template' or 'templateUrl' must presence all the time
+  styleUrls: ['./app.component.css'] // either 'template' or 'templateUrl' must presence all the time, and has to be in array
+  /*
+  you use inline style like: (*if your content is not a lots)
+  style: [`
+    h6{
+      color: firebrick;
+    }
+  `],
+  */
 })
 export class AppComponent {
   title = 'learn-angular-bf0904';
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,6 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
+  /*
+  you use inline template like: (*if your content is not a lots)
+  template: `
+  <app-server></app-server>
+  <app-server></app-server>
+  `,
+  */
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
