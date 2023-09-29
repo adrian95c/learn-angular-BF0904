1c58c23dad953235b05f6ac6221a0e3560e05f75
Adrian <adrian_chin95@outlook.com>
2023-09-29 22:42:25 +0800

    Work on both-way binding.
    Demonstrate the behaviour of both-way binding.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 // Angular will not scan through all component files in your app folder, unless you have register/ import here
@@ -13,6 +14,7 @@ import { ServersComponent } from './servers/servers.component';
     ServersComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
