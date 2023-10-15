be087aec01fe964e600a49383e6926f17ada972c
Adrian <adrian_chin95@outlook.com>
2023-10-15 16:56:21 +0800

    Output recipe list.
    Use ngFor to loop and display recipe info.

--- foodie-app-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ foodie-app-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit{
+  recipes: Recipe[] = [
+    new Recipe('Turkey Sandwich', 'A tender turkey meat with refreshing vegetables.', 'assets/images/Turkey_Sandwich.jpg')
+  ]
 
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
 }
