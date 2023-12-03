f481f720a83a28ce2733275ce681d66bc8a0b59a
Adrian <adrian_chin95@outlook.com>
2023-12-03 15:22:36 +0800

    Bind custom properties.
    Expose properties to outside component.

--- component-and-databinding/src/app/server-element/server-element.component.ts
+++ component-and-databinding/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
+  @Input() element!: {type: string, name: string, content: string}; 
+  // @Input() is to expose your variable globally, instead only internal usage
+  // Typescript 2.7.2 included a strict class checking where all properties should be declared in constructor, (!) exclamation mark for workaround
 
   constructor() {}
 
-  ngOnInit(){
-    
-  }
+  ngOnInit(){}
 }
