08fdb39bb9f2fb09f7e877ee8ab1edb8daf3d4f4
Adrian <adrian_chin95@outlook.com>
2023-12-30 13:19:50 +0800

    Work on ContentChild.
    Call Content outside the View, and display/ use it in the View.

--- component-and-databinding/src/app/server-element/server-element.component.ts
+++ component-and-databinding/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnChanges, Input, ViewEncapsulation, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, ViewEncapsulation, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy, ViewChild, ElementRef, ContentChild } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -13,6 +13,7 @@ export class ServerElementComponent implements OnInit, OnChanges, DoCheck, After
   // Typescript 2.7.2 included a strict class checking where all properties should be declared in constructor, (!) exclamation mark for workaround
   @Input() name!: string;
   @ViewChild('heading') header!: ElementRef;
+  @ContentChild('contentParagraph') paragraph!: ElementRef;
 
   constructor() {
     console.log('constructor called!');
@@ -26,21 +27,27 @@ export class ServerElementComponent implements OnInit, OnChanges, DoCheck, After
   ngOnInit(){
     console.log('ngOnInit called!')
     console.log('Text Content: ' + this.header.nativeElement.textContent); // This wont work because the DOM is yet initialise/ render, you wont have access/ use value/ content to template.
+    console.log('Text Content of Paragraph: ' + this.paragraph.nativeElement.textContent);
   }
 
   ngDoCheck(){
     console.log('ngDoCheck called!');
   }
 
+  // How to differentiate Content and View?
+  // Everything inside server-element called View
+  // Everything outside server-element called Content
+
   ngAfterContentInit(){
-    console.log('ngAfterContentInit called!'); // refer to the content belong to app.component
+    console.log('ngAfterContentInit called!'); // refer to the content in app.component
+    console.log('Text Content of Paragraph: ' + this.paragraph.nativeElement.textContent);
   }
 
   ngAfterContentChecked(){
     console.log('ngAfterContentChecked called!'); // Usually will be called after every check, like ngDoCheck
   }
 
-  // Both will be called after content being check
+  // Both will be called after server-element view being check
   ngAfterViewInit(){
     console.log('ngAfterViewInit called!');
     console.log('Text Content: ' + this.header.nativeElement.textContent);
