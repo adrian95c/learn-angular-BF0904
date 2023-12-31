cc7a19e0dad7004322e6d502bc45074381517520
Adrian <adrian_chin95@outlook.com>
2023-12-31 19:06:02 +0800

    6.6 Add ingredient
    Pass ingredient data to shopping list with ViewChild.

--- foodie-app-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ foodie-app-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent {
+export class ShoppingEditComponent implements OnInit{
 
+  @ViewChild('nameInput') nameInputRef!: ElementRef;
+  @ViewChild('amountInput') amountInputRef!: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
+  ngOnInit(): void {}
+
+  onAddItem(){
+    const ingredientName = this.nameInputRef.nativeElement.value;
+    const ingredientAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingredientName, ingredientAmount);
+
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
