49f06067cdb3e9b4ddcf8b41d1d7be16fdb423cb
Adrian <adrian_chin95@outlook.com>
2024-01-07 12:10:01 +0800

    7.7 Use HostListener

--- directives/src/app/better-highlight/better-highlight.directive.ts
+++ directives/src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, OnInit, Renderer2, ElementRef} from '@angular/core';
+import { Directive, OnInit, Renderer2, ElementRef, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -9,6 +9,15 @@ export class BetterHighlightDirective implements OnInit{
 
   ngOnInit(){
     // Renderer2 is recommended when you need to manipulate the DOM directly in Angular, as it provides a safer way to interact with the DOM compared to directly accessing with ElementRef.
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'LightPink');
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'LightPink');
+  }
+
+  // HostListener is a decorator to declares a DOM event to listen for and provides a handler method to run when that event occurs.
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'LightPink');  
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'Transparent');
   }
 }
